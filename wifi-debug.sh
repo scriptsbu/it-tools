9935645a5aca8b48e18d578b47782ef51997db60
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-20 16:47:20 -0500

    Update wifi-debug.sh

--- wifi-debug.sh
+++ wifi-debug.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-dmesg | grep -e wlp -e iwl  >  dmesg.txt
+dmesg | grep -e wlp -e iwl  >  wifi.txt
+scp wifi.txt ftpuser@10.20.240.3 /home/ftpuser/file_transfer
 
+#============================================================================
+#scp [source file] [username]@[destination server]
+#scp cool_stuff.txt sanjeev@example.com:.
+#filezilla sftp://ftpuser:torc.AUS?@10.20.240.16/home/ftpuser/file_transfer
 
 #MAKE THE FILE UPLOAD TO A PUBLIC SERVER
 #LOOK FOR PATTERNS OF WIFI AP CHANGING CONSTANTLY
