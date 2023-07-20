d112547f9fe18349bcb90ab38679c1ba4c70e8fd
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-20 16:50:06 -0500

    Update wifi-debug.sh

--- wifi-debug.sh
+++ wifi-debug.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 dmesg | grep -e wlp -e iwl  >  wifi.txt
-scp wifi.txt ftpuser@10.20.240.3 /home/ftpuser/file_transfer
+scp wifi.txt ftpuser@10.20.240.3:/home/ftpuser/file_transfer
 
 #============================================================================
 #scp [source file] [username]@[destination server]
 #scp cool_stuff.txt sanjeev@example.com:.
+scp cool_stuff.txt sanjeev@example.com:/this/path/right/here
 #filezilla sftp://ftpuser:torc.AUS?@10.20.240.16/home/ftpuser/file_transfer
 
 #MAKE THE FILE UPLOAD TO A PUBLIC SERVER
