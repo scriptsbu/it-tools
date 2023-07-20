7f892947463c52dcd9a9194e50080499bcdaf095
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-20 17:38:48 -0500

    Update wifit.sh

--- wifi.sh
+++ wifi.sh
@@ -5,7 +5,6 @@ scp wifi.txt wifi:WIFI.wifi123?@10.20.240.3:/debug/wifi
 #scp [source file] [username]@[destination server]
 #scp cool_stuff.txt sanjeev@example.com:.
 scp cool_stuff.txt sanjeev@example.com:/this/path/right/here
-#filezilla sftp://ftpuser:torc.AUS?@10.20.240.16/home/ftpuser/file_transfer
-
+#filezilla sftp://wifiWIFI.wifi123?@10.20.240.3/debug/wifi
 #MAKE THE FILE UPLOAD TO A PUBLIC SERVER
 #LOOK FOR PATTERNS OF WIFI AP CHANGING CONSTANTLY
