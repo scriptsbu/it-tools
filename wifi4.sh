b708a44940688ce465cb73e0be0573f548de6358
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-25 14:50:04 -0500

    Update wifi4.sh

--- wifi4.sh
+++ wifi4.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 dmesg | grep -e wlp -e iwl  >  wifi$USER.txt -f
-sshpass -p "WIFI.wifi123?" scp -r wifi.txt wifi@10.20.240.3:/debug/wifi/wifi$USER.txt
+sshpass -p "CHAGE" scp -r wifi.txt wifi@10.20.240.3:/debug/wifi/wifi$USER.txt
+
 #============================================================================
 #scp [source file] [username]@[destination server]
 #scp cool_stuff.txt sanjeev@example.com:.
 #scp cool_stuff.txt sanjeev@example.com:/this/path/right/here
-#filezilla sftp://wifiWIFI.wifi123?@10.20.240.3/debug/wifi
+#filezilla sftp://CHANGE@10.20.240.3/debug/wifi
 #MAKE THE FILE UPLOAD TO A PUBLIC SERVER
 #LOOK FOR PATTERNS OF WIFI AP CHANGING CONSTANTLY
