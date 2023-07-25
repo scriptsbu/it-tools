c8c8d09221fec0e34feedbfe176365ef3d11304b
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-25 16:49:17 -0500

    Update and rename wifi6.sh to wifi7.sh

--- wifi7.sh
+++ wifi7.sh
@@ -4,19 +4,19 @@ dmesg | grep -e wlp -e iwl  >  wifi$USER.txt
 
 while true;do
  
-
-  read -p "\x1b[41;37m Are you connected to TORC VPN?\x1b[K\x1b[0m" QUESTION
+   echo -e "\x1b[41;37m Are you connected to TORC VPN?\x1b[K\x1b[0m"
+   read -p "Y/N" QUESTION
   
   
   case "${QUESTION}" in
     [Yy] ) 
-      echo "\x1b[41;37m Uploading LOG\x1b[K\x1b[0m"
+      echo -e "\x1b[41;37m Uploading LOG\x1b[K\x1b[0m"
        scp -r wifi.txt wifi@10.20.240.3:/debug/wifi/wifi$USER.txt | bar && sleep 5 && echo -e "\x1b[41;37m LOG upload completed.\x1b[K\x1b[0m" && rm wifi$USER.txt
   
       ;;
 
     * ) 
-      echo "\x1b[41;37m Please connect to the VPN 'split push'\x1b[K\x1b[0m"
+      echo -e "\x1b[41;37m Please connect to the VPN 'split push'\x1b[K\x1b[0m"
       gnome-terminal -- /opt/cisco/anyconnect/bin/vpnui && echo -e "\x1b[41;37m Once connected to the VPN; Press [Enter] key to proceed.\x1b[K\x1b[0m" && read -p " " && ping -c1 10.20.240.3 && scp -r wifi.txt wifi@10.20.240.3:/debug/wifi/wifi$USER.txt && echo -e "\x1b[41;37m LOG upload completed.\x1b[K\x1b[0m" && rm wifi$USER.txt
 
       ;;
