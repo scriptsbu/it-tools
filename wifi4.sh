2fc87b6a4ae9bad84ed2346d27d9ea6416aa89bc
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-07-25 16:30:39 -0500

    Update wifi5.sh

--- wifi4.sh
+++ wifi4.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 dmesg | grep -e wlp -e iwl  >  wifi$USER.txt -f
-sshpass -p "CHANGE" scp -r wifi.txt wifi@10.20.240.3:/debug/wifi/wifi$USER.txt
+
+while true;do
+ 
+
+  read -p "Are you connected to TORC VPN?" QUESTION
+  
+  case "${QUESTION}" in
+    [Yy] ) 
+      echo "Uploading log"    
+       scp -r wifi.txt wifi@10.20.240.3:/debug/wifi/wifi$USER.txt |bar && sleep 5 && echo -e "\x1b[41;37m Log upload complete.\x1b[K\x1b[0m"
+  
+      ;;
+
+    * ) 
+      echo "Please connect to the VPN 'split push'"
+      gnome-terminal -- /opt/cisco/anyconnect/bin/vpnui && echo -e "\x1b[41;37m Press [Enter] key to proceed.\x1b[K\x1b[0m" && read -p " " && scp -r wifi.txt wifi@10.20.240.3:/debug/wifi/wifi$USER.txt
+
+      ;;
+  esac
+done
+
 
 #============================================================================
 #scp [source file] [username]@[destination server]
