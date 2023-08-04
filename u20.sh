8e12f4341e4fdce4bdc63a489bc4933f85c4dcd8
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-04 14:47:53 -0500

    Create u20.sh

---
+++ u20.sh
@@ -0,0 +1,9 @@
+#HOSTNAME FIX
+bash <(curl -Ls https://github.com/scriptsbu/scripts/raw/main/host.sh)
+#PAPERCUT INSTALL
+
+#SLACK
+
+#VMWARE HORIZON [VDI] SERVER: vdi.torc.tech
+
+#CISCO ANYCONNECT
