462b8963c0462f5ef423cce623f7f547d25cdff7
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-09 09:57:10 -0500

    Add files via upload

---
+++ it-storage/vmwareit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget http://10.20.240.3/it/VMware-Horizon-Client-2306-8.10.0-21964631.x64.deb
+sudo dpkg -i VMware-Horizon-Client-2306-8.10.0-21964631.x64.deb
+rm VMware-Horizon-Client-2306-8.10.0-21964631.x64.deb
+#--------------------------------------
+#VMware-Horizon-Client file stored at: t14-aus-it-server
