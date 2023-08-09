9cd573fee2f128493da71f110f1c8b28f77ec839
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-09 11:44:23 -0500

    Update papercutit.sh

--- it-software/papercutit.sh
+++ it-software/papercutit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+sudo apt update -y
 wget http://10.20.240.3/it/pc-print-deploy-client[papercut.torc.tech].deb
 sudo dpkg -i pc-print-deploy-client[papercut.torc.tech].deb
 rm pc-print-deploy-client[papercut.torc.tech].deb
-/opt/PaperCutPrintDeployClient/initialise.sh -w 
+/opt/PaperCutPrintDeployClient/initialise.sh -w
+sudo apt upgrade -y
 #--------------------------------------
 #Papercut file stored at: t14-aus-it-server
