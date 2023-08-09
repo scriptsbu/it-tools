4e896467e78265d3d8351ebf808d6f279a5acc38
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-09 11:44:07 -0500

    Update slackit.sh

--- it-software/slackit.sh
+++ it-software/slackit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+sudo apt update -y
 wget http://10.20.240.3/it/slack.deb
 sudo dpkg -i slack.deb
 rm slack.deb -f
+sudo apt upgrade -y
 #--------------------------------------
 #Slack file stored at: t14-aus-it-server
