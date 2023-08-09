ca3c2e71a3c0178b16f14b131f75237f14909e41
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-09 11:43:50 -0500

    Update vmwareit.sh

--- it-software/vmwareit.sh
+++ it-software/vmwareit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+sudo apt update -y
 wget http://10.20.240.3/it/VMware-Horizon-Client-2306-8.10.0-21964631.x64.deb
 sudo dpkg -i VMware-Horizon-Client-2306-8.10.0-21964631.x64.deb
 rm VMware-Horizon-Client-2306-8.10.0-21964631.x64.deb
+sudo apt upgrade -y
 #--------------------------------------
 #VMware-Horizon-Client file stored at: t14-aus-it-server
