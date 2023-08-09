c015363af129ef9519ea44c8d36093aab995a720
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-09 12:23:31 -0500

    Update vmwareit.sh

--- it-software/vmwareit.sh
+++ it-software/vmwareit.sh
@@ -4,5 +4,7 @@ wget http://10.20.240.3/it/VMware-Horizon-Client-2306-8.10.0-21964631.x64.deb
 sudo dpkg -i VMware-Horizon-Client-2306-8.10.0-21964631.x64.deb
 rm VMware-Horizon-Client-2306-8.10.0-21964631.x64.deb
 sudo apt upgrade -y
+echo -e "\x1b[41;37mOpen VDI and add server: vdi.torc.tech.\x1b[K\x1b[0m"
+sleep 30 && exit
 #--------------------------------------
 #VMware-Horizon-Client file stored at: t14-aus-it-server
