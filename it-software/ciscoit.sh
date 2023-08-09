2d5ba23ff86083ac5e34ec8aab54f45e9c6cc6b3
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-08-09 11:44:52 -0500

    Update ciscoit.sh

--- it-software/ciscoit.sh
+++ it-software/ciscoit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt update -y
 wget https://github.com/scriptsbu/software/raw/main/storage/anyconnect-linux64-4.10.01075-core-vpn-webdeploy-k9.sh
 sudo bash anyconnect-linux64-4.10.01075-core-vpn-webdeploy-k9.sh && sleep 2 m && sudo rm -r anyconnect-linux64-4.10.01075-core-vpn-webdeploy-k9.sh -f
-sleep 10
+sudo apt upgrade -y
