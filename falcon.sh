90f40acf567dcf37929d06474dc0d2b17dd95c95
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-12-08 14:29:16 -0600

    Create falcon.sh

---
+++ falcon.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt update
+cd ~/Downloads
+wget http://10.20.240.3/it/falcon-sensor_7.04.0-15907_amd64.deb
+sudo dpkg -i falcon-sensor_7.04.0-15907_amd64.deb
+sudo apt-get upgrade -y
