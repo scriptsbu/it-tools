c65bf9567159a6c50bd711a2ca4a36972df23895
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-12-14 13:46:43 -0600

    Update papercutit.sh

--- it-software/papercutit.sh
+++ it-software/papercutit.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+#REMOVING OLD FILES
 cd ~/Downloads
-bash /opt/PaperCutPrintDeployClient/uninitialise.sh -w
 sudo apt-get remove papercutprintdeployclient -y -f
 sudo apt-get purge papercutprintdeployclient -y -f
-rm pc-print-deploy-client[papercut.torc.tech].deb -f
+sudo rm pc-print-deploy-client[papercut.torc.tech].deb -f
 sudo rm -r /opt/PaperCutPrintDeployClient -f
+#REMOVING OLD CONFIG
+cd /var/lib/dpkg/info/
+ls | grep papercut
+sudo rm -r papercut-print-deploy-client.conffiles
+sudo rm -r papercut-print-deploy-client.list
+sudo rm -r papercut-print-deploy-client.md5sums
+sudo rm -r papercut-print-deploy-client.postinst
+sudo rm -r papercut-print-deploy-client.prerm
+#INSTALLING PAPERCUT
+cd ~/Downloads
 sudo apt update
 echo -e "\x1b[41;37mTo finalise the installation, please exit Papercut when it starts; press [ENTER] to proceed.\x1b[K\x1b[0m"
 read -p ""
@@ -14,7 +24,7 @@ rm pc-print-deploy-client[papercut.torc.tech].deb
 sudo apt upgrade -y
 sudo bash /opt/PaperCutPrintDeployClient/initialise.sh -w
 sudo dpkg -l | grep papercut
-#--------------------------------------
+#-------------TROUBLESHOOTING-BACKUP-------------------------
 #Papercut file stored at: t14-aus-it-server
 #If PaperCut shows an error after installation navigate to:
 #nano /var/lib/dpkg/status
