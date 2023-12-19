786836bb2bf6690a4de7beca81076d174b51e325
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2023-12-19 10:51:17 -0600

    Update falcon.sh

--- falcon.sh
+++ falcon.sh
@@ -3,4 +3,4 @@ sudo apt update
 cd ~/Downloads
 wget http://10.20.240.3/it/falcon-sensor_7.04.0-15907_amd64.deb
 sudo dpkg -i falcon-sensor_7.04.0-15907_amd64.deb
-sudo apt-get upgrade -y
+#sudo apt-get upgrade -y
