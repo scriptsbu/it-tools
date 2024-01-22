6083d0618f85ff04c866afba442b982dff1a45c6
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2024-01-22 11:13:02 -0600

    Update delldisplaylink.sh

--- delldisplaylink.sh
+++ delldisplaylink.sh
@@ -3,4 +3,4 @@ cd ~/Downloads
 wget http://10.20.240.3/it/displaylink-driver-5.6.0-59.176.run
 sudo chmod +x displaylink-driver-5.6.0-59.176.run
 ./displaylink-driver-5.6.0-59.176.run
-sudo rm -r displaylink-driver-5.6.0-59.176.run -y
+sudo rm displaylink-driver-5.6.0-59.176.run -y
