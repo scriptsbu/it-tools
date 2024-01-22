9567845a4b5837898c8e8b018db00ecabfb82cc1
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2024-01-22 11:11:30 -0600

    Create delldisplaylink.sh

---
+++ delldisplaylink.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~/Downloads
+wget http://10.20.240.3/it/displaylink-driver-5.6.0-59.176.run
+sudo chmod +x displaylink-driver-5.6.0-59.176.run
+./displaylink-driver-5.6.0-59.176.run
+sudo rm -r displaylink-driver-5.6.0-59.176.run -y
