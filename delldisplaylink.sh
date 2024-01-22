b00e174920b25b11aa39652bed0b214c2539d645
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2024-01-22 11:39:25 -0600

    Update delldisplaylink.sh

--- delldisplaylink.sh
+++ delldisplaylink.sh
@@ -2,5 +2,23 @@
 cd ~/Downloads
 wget http://10.20.240.3/it/displaylink-driver-5.6.0-59.176.run
 sudo chmod +x displaylink-driver-5.6.0-59.176.run
-./displaylink-driver-5.6.0-59.176.run
+sudo apt install dkms
+sudo apt install evdi-dkms
+sudo ./displaylink-driver-5.6.0-59.176.run
 sudo rm displaylink-driver-5.6.0-59.176.run -y
+while true;do
+ 
+
+  read -p "Do you want to reboot now(Y/N)? " QUESTION
+  case "${QUESTION}" in
+    [Yy] ) 
+      echo "Rebooting now..."    
+       sudo reboot  
+      ;;
+
+    * ) 
+      echo "Don't forget to Reboot the system to apply changes!"
+      exit
+      ;;
+  esac
+done
