0d166728e02007223f12b8dcb7e1a912292e7d8b
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2024-01-26 13:03:51 -0600

    Update landscape.sh

--- landscape.sh
+++ landscape.sh
@@ -3,4 +3,4 @@
 cd ~/Downloads
 wget http://10.20.240.3/it/script/install_landscape_and_prov2.sh
 sudo bash install_landscape_and_prov2.sh
-sudo rm install_landscape_and_prov2.sh
+sudo rm -r install_landscape_and_prov2.sh
