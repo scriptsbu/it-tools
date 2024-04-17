ffc61960c4e6b8f7b9b249c33f97c7e96c90584d
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2024-04-17 14:16:55 -0500

    Update tags.sh

--- tags.sh
+++ tags.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
-echo "tags = ${region},${brand},${model},${form}" >> /etc/landscape/client.conf
+read -p "Enter Region (usa,str,mtl):" region
+read -p "Enter Computer make (dell,lenovo):" brand
+read -p "Enter Computer model (l5420,T14):" model
+read -p "Enter Computer form factor(laptop,desktop):" form
+sudo bash -c 'echo "tags = ${region},${brand},${model},${form}" >> /etc/landscape/client.conf'
