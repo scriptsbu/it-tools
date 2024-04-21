169808b255e53bc3e403f2de4cc214ad8caeca3c
scriptsbu <137096347+scriptsbu@users.noreply.github.com>
2024-04-21 12:02:52 -0500

    Update landscape.sh

--- landscape.sh
+++ landscape.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+echo "Please connect to the VPN before executing this script"
+read -p "Press the ENTER key to proceed or CTRL + C to cancel."
 #bash <(curl -Ls http://10.20.240.3/it/script/install_landscape_and_prov2.sh)
 cd ~/Downloads
 wget http://10.20.240.3/it/script/install_landscape_and_prov2.sh
