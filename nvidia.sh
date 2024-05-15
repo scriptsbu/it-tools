e37a5f37eaf2410d623a70483c6171335fd80520
Alberto Lopez-Santiago <137096347+scriptsbu@users.noreply.github.com>
2024-05-15 10:59:48 -0500

    Create nvidia.sh

---
+++ nvidia.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#verify GPU installed:
+hwinfo --gfxcard --short
+#install nvidia driver:
+sudo apt-get install libnvidia-extra-535
+sudo apt install nvidia-driver-535 nvidia-dkms-535
+sudo ubuntu-drivers devices
+sudo ubuntu-drivers install
+#Verify nvidia driver
+nvidia-smi
